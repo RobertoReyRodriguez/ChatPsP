561b99bf5f873b36b59a662138caed132b62523b
nrey <natalia.rey.lorono@gmail.com>
2025-01-22 17:57:13 +0100

    Update Javadoc

--- src/chatcliente/Cliente.java
+++ src/chatcliente/Cliente.java
@@ -1,9 +1,3 @@
-/*
- * Ejemplo desarrollado por Erick Navarro
- * Blog: e-navarro.blogspot.com
- * Noviembre - 2015
- */
-
 package chatcliente;
 
 import java.io.*;
@@ -13,106 +7,91 @@ import java.util.LinkedList;
 import javax.swing.JOptionPane;
 
 /**
- * Clase en la que se maneja la comunicación del lado del cliente.
- * @author Erick Navarro
+ * Clase para gestionar el cliente.
  */
 public class Cliente extends Thread {
-    /**
-     * Socket utilizado para comunicarse con el servidor.
-     */
+    /** Socket para comunicarse con el servidor. */
     private Socket socket;
-    /**
-     * Stream utilizado para el envío de objetos al servidor.
-     */    
+    /** Stream para el envío de objetos al servidor. */
     private ObjectOutputStream objectOutputStream;
-    /**
-     * Stream utilizado para el envío de objetos al servidor.
-     */    
+    /** Stream para el envío de objetos al servidor. */
     private ObjectInputStream objectInputStream;
-    /**
-     * Ventana utilizada para la interfaz gráfica del cliente.
-     */        
-    private final VentanaC ventana;    
-    /**
-     * Identificador único del cliente dentro del chat.
-     */
+    /** Ventana para la interfaz gráfica del cliente. */
+    private final VentanaC ventana;
+    /** Identificador único del cliente dentro del chat. */
     private String identificador;
-    /**
-     * Variable que determina si el cliente escucha o no al servidor, una vez 
-     * que se arranca el hilo de comunicación del cliente.
-     */
+    /** Determina si el cliente escucha o no al servidor */
     private boolean escuchando;
-    /**
-     * Variable que almacena la IP del host en el que se ejecuta el servidor.
-     */
+    /** Almacena la IP del host en el que se ejecuta el servidor. */
     private final String host;
-    /**
-     * Varable que almacena el puerto por el cual el servidor escucha las conexiones
-     * de los diversos clientes.
-     */
+    /** Puerto por el cual el servidor escucha las conexiones de los clientes. */
     private final int puerto;
+
     /**
-     * Constructor de la clase cliente.
+     * Constructor de la clase
+     * 
      * @param ventana
      * @param host
      * @param puerto
-     * @param nombre 
+     * @param nombre
      */
     Cliente(VentanaC ventana, String host, Integer puerto, String nombre) {
-        this.ventana=ventana;        
-        this.host=host;
-        this.puerto=puerto;
-        this.identificador=nombre;
-        escuchando=true;
+        this.ventana = ventana;
+        this.host = host;
+        this.puerto = puerto;
+        this.identificador = nombre;
+        escuchando = true;
         this.start();
     }
+
     /**
      * Método run del hilo de comunicación del lado del cliente.
      */
-    public void run(){
+    public void run() {
         try {
-            socket=new Socket(host, puerto);
-            objectOutputStream=new ObjectOutputStream(socket.getOutputStream());
-            objectInputStream=new ObjectInputStream(socket.getInputStream());
+            socket = new Socket(host, puerto);
+            objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+            objectInputStream = new ObjectInputStream(socket.getInputStream());
             System.out.println("Conexion exitosa!!!!");
             this.enviarSolicitudConexion(identificador);
             this.escuchar();
         } catch (UnknownHostException ex) {
             JOptionPane.showMessageDialog(ventana, "Conexión rehusada, servidor desconocido,\n"
-                                                 + "puede que haya ingresado una ip incorrecta\n"
-                                                 + "o que el servidor no este corriendo.\n"
-                                                 + "Esta aplicación se cerrará.");
+                    + "puede que haya ingresado una ip incorrecta\n"
+                    + "o que el servidor no este corriendo.\n"
+                    + "Esta aplicación se cerrará.");
             System.exit(0);
         } catch (IOException ex) {
             JOptionPane.showMessageDialog(ventana, "Conexión rehusada, error de Entrada/Salida,\n"
-                                                 + "puede que haya ingresado una ip o un puerto\n"
-                                                 + "incorrecto, o que el servidor no este corriendo.\n"
-                                                 + "Esta aplicación se cerrará.");
+                    + "puede que haya ingresado una ip o un puerto\n"
+                    + "incorrecto, o que el servidor no este corriendo.\n"
+                    + "Esta aplicación se cerrará.");
             System.exit(0);
         }
 
     }
-    
+
     /**
      * Método que cierra el socket y los streams de comunicación.
      */
-    public void desconectar(){
+    public void desconectar() {
         try {
             objectOutputStream.close();
             objectInputStream.close();
-            socket.close();  
-            escuchando=false;
+            socket.close();
+            escuchando = false;
         } catch (Exception e) {
             System.err.println("Error al cerrar los elementos de comunicación del cliente.");
         }
     }
+
     /**
-     * Método que envia un determinado mensaje hacia el servidor.
+     * Método que envia un mensaje hacia el servidor.
      * @param cliente_receptor
-     * @param mensaje 
+     * @param mensaje
      */
-    public void enviarMensaje(String cliente_receptor, String mensaje){
-        LinkedList<String> lista=new LinkedList<>();
+    public void enviarMensaje(String cliente_receptor, String mensaje) {
+        LinkedList<String> lista = new LinkedList<>();
         lista.add("MENSAJE");
         lista.add(identificador);
         lista.add(cliente_receptor);
@@ -123,17 +102,17 @@ public class Cliente extends Thread {
             System.out.println("Error de lectura y escritura al enviar mensaje al servidor.");
         }
     }
-    
+
     /*
      * Método que escucha constantemente lo que el servidor dice.
-     */    
+     */
     public void escuchar() {
         try {
             while (escuchando) {
                 Object aux = objectInputStream.readObject();
                 if (aux != null) {
                     if (aux instanceof LinkedList) {
-                        ejecutar((LinkedList<String>)aux);
+                        ejecutar((LinkedList<String>) aux);
                     } else {
                         System.err.println("Se recibió un Objeto desconocido a través del socket");
                     }
@@ -143,22 +122,24 @@ public class Cliente extends Thread {
             }
         } catch (Exception e) {
             JOptionPane.showMessageDialog(ventana, "La comunicación con el servidor se ha\n"
-                                                 + "perdido, este chat tendrá que finalizar.\n"
-                                                 + "Esta aplicación se cerrará.");
+                    + "perdido, este chat tendrá que finalizar.\n"
+                    + "Esta aplicación se cerrará.");
             System.exit(0);
         }
     }
+
     /**
-     * Método que ejecuta una serie de instruccines dependiendo del mensaje que el cliente reciba del servidor.
+     * Método que ejecuta una serie de instruccines dependiendo del mensaje que el
+     * cliente reciba del servidor.
      * @param lista
      */
-    public void ejecutar(LinkedList<String> lista){
-        String tipo=lista.get(0);
+    public void ejecutar(LinkedList<String> lista) {
+        String tipo = lista.get(0);
         switch (tipo) {
             case "CONEXION_ACEPTADA":
-                identificador=lista.get(1);
+                identificador = lista.get(1);
                 ventana.sesionIniciada(identificador);
-                for(int i=2;i<lista.size();i++){
+                for (int i = 2; i < lista.size(); i++) {
                     ventana.addContacto(lista.get(i));
                 }
                 break;
@@ -167,7 +148,7 @@ public class Cliente extends Thread {
                 break;
             case "USUARIO_DESCONECTADO":
                 ventana.eliminarContacto(lista.get(1));
-                break;                
+                break;
             case "MENSAJE":
                 ventana.addMensaje(lista.get(1), lista.get(3));
                 break;
@@ -175,13 +156,13 @@ public class Cliente extends Thread {
                 break;
         }
     }
+
     /**
-     * Al conectarse el cliente debe solicitar al servidor que lo agregue a la 
-     * lista de clientes, para ello se ejecuta este método.
-     * @param identificador 
+     * Método para solicitar la agregación del cliente a la lista de clientes
+     * @param identificador
      */
     private void enviarSolicitudConexion(String identificador) {
-        LinkedList<String> lista=new LinkedList<>();
+        LinkedList<String> lista = new LinkedList<>();
         lista.add("SOLICITUD_CONEXION");
         lista.add(identificador);
         try {
@@ -190,13 +171,13 @@ public class Cliente extends Thread {
             System.out.println("Error de lectura y escritura al enviar mensaje al servidor.");
         }
     }
+
     /**
-     * Cuando se cierra una ventana cliente, se debe notificar al servidor que el 
-     * cliente se ha desconectado para que lo elimine de la lista de clientes y 
-     * todos los clientes lo eliminen de su lista de contactos.
+     * Tramita una notificación de desconexión para eliminar al cliente 
+     * de las listas de contactos y de la lista de clientes
      */
     void confirmarDesconexion() {
-        LinkedList<String> lista=new LinkedList<>();
+        LinkedList<String> lista = new LinkedList<>();
         lista.add("SOLICITUD_DESCONEXION");
         lista.add(identificador);
         try {
@@ -205,9 +186,10 @@ public class Cliente extends Thread {
             System.out.println("Error de lectura y escritura al enviar mensaje al servidor.");
         }
     }
+
     /**
-     * Método que retorna el identificador del cliente que es único dentro del chat.
-     * @return 
+     * Devuelve el identificador único del cliente
+     * @return identificador del cliente
      */
     String getIdentificador() {
         return identificador;
--- src/chatcliente/VentanaC.java
+++ src/chatcliente/VentanaC.java
@@ -1,19 +1,25 @@
-
-
 package chatcliente;
 
 import java.awt.GridLayout;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import java.awt.event.*;
+import javax.swing.*;
 
 /**
  * Clase que maneja la interfaz gráfica del cliente.
- * 
  */
-public class VentanaC extends javax.swing.JFrame {
+public class VentanaC extends JFrame {
+    private JButton btnEnviar;
+    private JComboBox cmbContactos;
+    private JLabel jLabel1;
+    private JScrollPane jScrollPane1;
+    private JTextArea txtHistorial;
+    private JTextField txtMensaje;
+    /** Puerto por defecto para la aplicación. */
+    private final String DEFAULT_PORT = "10101";
+    /** IP por defecto (localhost) para el servidor. */
+    private final String DEFAULT_IP = "127.0.0.1";
+    /** Almacena el cliente */
+    private final Cliente cliente;
 
     /**
      * Constructor de la ventana.
@@ -21,33 +27,32 @@ public class VentanaC extends javax.swing.JFrame {
     public VentanaC() {
         initComponents();
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        String ip_puerto_nombre[]=getIP_Puerto_Nombre();
-        String ip=ip_puerto_nombre[0];
-        String puerto=ip_puerto_nombre[1];
-        String nombre=ip_puerto_nombre[2];
-        cliente=new Cliente(this, ip, Integer.valueOf(puerto), nombre);
+        String ip_puerto_nombre[] = getIP_Puerto_Nombre();
+        String ip = ip_puerto_nombre[0];
+        String puerto = ip_puerto_nombre[1];
+        String nombre = ip_puerto_nombre[2];
+        cliente = new Cliente(this, ip, Integer.valueOf(puerto), nombre);
     }
 
     /**
      * Método para inciar componentes y formulario.
      */
-    @SuppressWarnings("unchecked")
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        txtHistorial = new javax.swing.JTextArea();
-        txtMensaje = new javax.swing.JTextField();
-        cmbContactos = new javax.swing.JComboBox();
-        btnEnviar = new javax.swing.JButton();
-        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new JScrollPane();
+        txtHistorial = new JTextArea();
+        txtMensaje = new JTextField();
+        cmbContactos = new JComboBox();
+        btnEnviar = new JButton();
+        jLabel1 = new JLabel();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        addWindowListener(new java.awt.event.WindowAdapter() {
-            public void windowClosed(java.awt.event.WindowEvent evt) {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosed(WindowEvent evt) {
                 formWindowClosed(evt);
             }
-            public void windowClosing(java.awt.event.WindowEvent evt) {
+
+            public void windowClosing(WindowEvent evt) {
                 formWindowClosing(evt);
             }
         });
@@ -58,90 +63,95 @@ public class VentanaC extends javax.swing.JFrame {
         jScrollPane1.setViewportView(txtHistorial);
 
         btnEnviar.setText("Enviar");
-        btnEnviar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        btnEnviar.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
                 btnEnviarActionPerformed(evt);
             }
         });
 
         jLabel1.setText("Destinatario:");
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(txtMensaje)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                        .addComponent(btnEnviar))
-                    .addComponent(jScrollPane1)
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(cmbContactos, javax.swing.GroupLayout.PREFERRED_SIZE, 198, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap())
-        );
+                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addComponent(txtMensaje)
+                                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                                .addComponent(btnEnviar))
+                                        .addComponent(jScrollPane1)
+                                        .addGroup(GroupLayout.Alignment.TRAILING, layout
+                                                .createSequentialGroup()
+                                                .addComponent(jLabel1)
+                                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED,
+                                                        GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                                .addComponent(cmbContactos, GroupLayout.PREFERRED_SIZE, 198,
+                                                        GroupLayout.PREFERRED_SIZE)))
+                                .addContainerGap()));
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 254, Short.MAX_VALUE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(cmbContactos, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(txtMensaje, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnEnviar))
-                .addContainerGap())
-        );
-
+                layout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addContainerGap()
+                                .addComponent(jScrollPane1, GroupLayout.DEFAULT_SIZE, 254, Short.MAX_VALUE)
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                        .addComponent(cmbContactos, GroupLayout.PREFERRED_SIZE,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(jLabel1))
+                                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                .addGroup(layout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                        .addComponent(txtMensaje, GroupLayout.PREFERRED_SIZE,
+                                                GroupLayout.DEFAULT_SIZE,
+                                                GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(btnEnviar))
+                                .addContainerGap()));
         pack();
     }
+
     /**
-     * Al hacer clic en el botón de enviar, se debe pedir al cliente del chat que
-     * envíe al servidor el mensaje.
-     * @param evt 
+     * Al hacer clic en el botón de enviar, pide al cliente que envíe al servidor el mensaje.
+     * @param evt
      */
-    private void btnEnviarActionPerformed(java.awt.event.ActionEvent evt) {
-        if(cmbContactos.getSelectedItem()==null){
+    private void btnEnviarActionPerformed(ActionEvent evt) {
+        if (cmbContactos.getSelectedItem() == null) {
             JOptionPane.showMessageDialog(this, "Debe escoger un destinatario válido, si no \n"
-                                                 + "hay uno, espere a que otro usuario se conecte\n"
-                                                 + "para poder chatear con él.");        
+                    + "hay uno, espere a que otro usuario se conecte\n"
+                    + "para poder chatear con él.");
             return;
         }
-        String cliente_receptor=cmbContactos.getSelectedItem().toString();
-        String mensaje=txtMensaje.getText();
+        String cliente_receptor = cmbContactos.getSelectedItem().toString();
+        String mensaje = txtMensaje.getText();
         cliente.enviarMensaje(cliente_receptor, mensaje);
-        txtHistorial.append("## Yo -> "+cliente_receptor+ " ## : \n" + mensaje+"\n");
+        txtHistorial.append("## Yo -> " + cliente_receptor + " ## : \n" + mensaje + "\n");
         txtMensaje.setText("");
     }
 
-    private void formWindowClosed(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosed
+    private void formWindowClosed(WindowEvent evt) {
     }
+
     /**
      * Cuando la ventana se este cerrando se notifica al servidor que el cliente
      * se ha desconectado, por lo que los demás clientes del chat no podrán enviarle
      * más mensajes.
-     * @param evt 
+     * @param evt
      */
-    private void formWindowClosing(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosing
+    private void formWindowClosing(WindowEvent evt) {
         cliente.confirmarDesconexion();
     }
 
     /**
-     * @param args the command line arguments
+     * Main
+     * @param args
      */
     public static void main(String args[]) {
-       
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    UIManager.setLookAndFeel(info.getClassName());
                     break;
                 }
             }
@@ -151,12 +161,10 @@ public class VentanaC extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(VentanaC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
             java.util.logging.Logger.getLogger(VentanaC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+        } catch (UnsupportedLookAndFeelException ex) {
             java.util.logging.Logger.getLogger(VentanaC.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
-        //</editor-fold>
-
-        /* Create and display the form */
+ 
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new VentanaC().setVisible(true);
@@ -164,59 +172,41 @@ public class VentanaC extends javax.swing.JFrame {
         });
     }
 
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnEnviar;
-    private javax.swing.JComboBox cmbContactos;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTextArea txtHistorial;
-    private javax.swing.JTextField txtMensaje;
-    // End of variables declaration//GEN-END:variables
-    /**
-     * Constante que almacena el puerto por defecto para la aplicación.
-     */
-    private final String DEFAULT_PORT="10101";
-    /**
-     * Constante que almacena la IP por defecto (localhost) para el servidor.
-     */    
-    private final String DEFAULT_IP="127.0.0.1";
-    /**
-     * Constante que almacena el cliente, con el cual se gestiona la comunicación 
-     * con el servidor.
-     */
-    private final Cliente cliente;
     /**
      * Agrega un contacto al JComboBox de contactos.
-     * @param contacto 
+     * @param contacto
      */
-    void addContacto(String contacto) {
+    public void addContacto(String contacto) {
         cmbContactos.addItem(contacto);
     }
+
     /**
      * Agrega un nuevo mensaje al historial de la conversación.
      * @param emisor
-     * @param mensaje 
+     * @param mensaje
      */
-    void addMensaje(String emisor, String mensaje) {
-        txtHistorial.append("##### "+emisor + " ##### : \n" + mensaje+"\n");
+    public void addMensaje(String emisor, String mensaje) {
+        txtHistorial.append("##### " + emisor + " ##### : \n" + mensaje + "\n");
     }
+
     /**
      * Se configura el título de la ventana para una nueva sesión.
-     * @param identificador 
+     * @param identificador
      */
-    void sesionIniciada(String identificador) {
-        this.setTitle(" --- "+identificador+" --- ");
+    public void sesionIniciada(String identificador) {
+        this.setTitle(" --- " + identificador + " --- ");
     }
+
     /**
-     * Método que abre una ventana para que el usuario ingrese la IP del host en 
-     * el que corre el servidor, el puerto con el que escucha y el nombre con el 
+     * Método que abre una ventana para que el usuario ingrese la IP del host en
+     * el que corre el servidor, el puerto con el que escucha y el nombre con el
      * que quiere participar en el chat.
-     * @return 
+     * @return array con el puerto, la ip y el nombre
      */
     private String[] getIP_Puerto_Nombre() {
-        String s[]=new String[3];
-        s[0]=DEFAULT_IP;
-        s[1]=DEFAULT_PORT;
+        String s[] = new String[3];
+        s[0] = DEFAULT_IP;
+        s[1] = DEFAULT_PORT;
         JTextField ip = new JTextField(20);
         JTextField puerto = new JTextField(20);
         JTextField usuario = new JTextField(20);
@@ -230,26 +220,27 @@ public class VentanaC extends javax.swing.JFrame {
         myPanel.add(new JLabel("Puerto de la conexión:"));
         myPanel.add(puerto);
         myPanel.add(new JLabel("Escriba su nombre:"));
-        myPanel.add(usuario);        
-        int result = JOptionPane.showConfirmDialog(null, myPanel, 
-                 "Configuraciones de la comunicación", JOptionPane.OK_CANCEL_OPTION);
+        myPanel.add(usuario);
+        int result = JOptionPane.showConfirmDialog(null, myPanel,
+                "Configuraciones de la comunicación", JOptionPane.OK_CANCEL_OPTION);
         if (result == JOptionPane.OK_OPTION) {
-                s[0]=ip.getText();
-                s[1]=puerto.getText();
-                s[2]=usuario.getText();
-        }else{
+            s[0] = ip.getText();
+            s[1] = puerto.getText();
+            s[2] = usuario.getText();
+        } else {
             System.exit(0);
         }
         return s;
-    }    
+    }
+
     /**
-     * Método que elimina cierto cliente de la lista de contactos, este se llama
-     * cuando cierto usuario cierra sesión.
-     * @param identificador 
+     * Método que elimina un cliente de la lista de contactos, este se llama
+     * cuando un usuario cierra sesión.
+     * @param identificador
      */
     void eliminarContacto(String identificador) {
         for (int i = 0; i < cmbContactos.getItemCount(); i++) {
-            if(cmbContactos.getItemAt(i).toString().equals(identificador)){
+            if (cmbContactos.getItemAt(i).toString().equals(identificador)) {
                 cmbContactos.removeItemAt(i);
                 return;
             }
